de13a9e729483eff50a64f2396d79970cde4d74c
grian <grianguy32@gmail.com>
2024-08-17 00:46:22 +0300

    day1

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is my personal Advent of Code repo!
 
 ### Current progress
 
-- 2023: 0/50 Stars
+- 2023: 2/50 Stars
 - 2022: 0/50 Stars
 - 2021: 0/50 Stars
 - 2020: 0/50 Stars
@@ -13,4 +13,4 @@ This is my personal Advent of Code repo!
 - 2017: 0/50 Stars
 - 2016: 0/50 Stars
 - 2015: 0/50 Stars
-- Total: 0/450 Stars
+- Total: 2/450 Stars
--- src/main/kotlin/year2023/Day1.kt
+++ src/main/kotlin/year2023/Day1.kt
@@ -1,15 +1,65 @@
 package me.grian.year2023
 
 import me.grian.Day
+import me.grian.util.getInputText
 
 class Day1 : Day {
-    override val input: String = "return test"
+    override val input: String = getInputText(1)
 
     override fun partOne(): String {
-        return "part one test"
+        val lines = input.lines()
+        var sum = 0
+
+        lines.forEach { line ->
+            val digits = line.filter { it.isDigit() }
+            // can't add chars
+            sum += "${digits.first()}${digits.last()}".toInt()
+        }
+
+        return sum.toString()
     }
 
     override fun partTwo(): String {
-        return "part two test"
+        var text = input
+        var sum = 0
+
+        val overlapMap = mapOf(
+            "twone" to "twoone",
+            "oneight" to "oneeight",
+            "threeight" to "threeeight",
+            "fiveight" to "fiveeight",
+            "nineight" to "nineeight",
+            "sevenine" to "sevennine",
+            "eightwo" to "eighttwo",
+            "eighthree" to "eightthree"
+        )
+
+        val numberMap = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9
+        )
+
+        overlapMap.forEach {
+            text = text.replace(it.key, it.value)
+        }
+
+        numberMap.forEach {
+            text = text.replace(it.key, it.value.toString())
+        }
+
+        text.lines().forEach { line ->
+            val digits = line.filter { it.isDigit() }
+            // can't add chars
+            sum += "${digits.first()}${digits.last()}".toInt()
+        }
+
+        return sum.toString()
     }
 }
