6f2794f25350719ca0832eee7697a5c455687c62
grian <grianguy32@gmail.com>
2024-08-17 00:13:20 +0300

    update getinput

--- src/main/kotlin/util/Util.kt
+++ src/main/kotlin/util/Util.kt
@@ -3,4 +3,4 @@ package me.grian.util
 import java.nio.file.Path
 import kotlin.io.path.readText
 
-fun getInputFile(day: Int): String = Path.of("src/main/resources/inputs/day${day}").readText()
+fun getInputText(day: Int): String = Path.of("src/main/resources/inputs/day${day}.txt").readText()
