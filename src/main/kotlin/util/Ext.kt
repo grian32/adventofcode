39f982c807d612e3a9a96ab93249de9dfc9d18a6
grian <grianguy32@gmail.com>
2024-08-18 01:24:32 +0300

    day 2 part 2

---
+++ src/main/kotlin/util/Ext.kt
@@ -0,0 +1,10 @@
+package me.grian.util
+
+fun List<Int>.mul(): Int {
+    var total = this[0]
+
+    val listWithoutFirst = this.subList(1, this.size)
+    listWithoutFirst.forEach { total *= it }
+
+    return total
+}
--- src/main/kotlin/year2023/day2/Day2.kt
+++ src/main/kotlin/year2023/day2/Day2.kt
@@ -2,6 +2,7 @@ package me.grian.year2023.day2
 
 import me.grian.Day
 import me.grian.util.getInputText
+import me.grian.util.mul
 
 class Day2 : Day {
     override val input: String = getInputText(2)
@@ -24,7 +25,25 @@ class Day2 : Day {
     }
 
     override fun partTwo(): String {
-        TODO("Not yet implemented")
+        val parsedInput = parse()
+        var sum = 0
+
+        parsedInput.forEach { allShowings ->
+            // idx 0 = red, idx 1 = green, idx 2 = blue
+            val highestAmounts = mutableListOf(0, 0, 0)
+
+            allShowings.forEach {
+                when (it.color) {
+                    "red" -> if (it.amount > highestAmounts[0]) highestAmounts[0] = it.amount
+                    "green" -> if (it.amount > highestAmounts[1]) highestAmounts[1] = it.amount
+                    "blue" -> if (it.amount > highestAmounts[2]) highestAmounts[2] = it.amount
+                }
+            }
+
+            sum += highestAmounts.mul()
+        }
+
+        return sum.toString()
     }
 
     private fun parse(): List<List<CubeSet>> {
