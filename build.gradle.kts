a80fc4acd76d9819e3d5e46703070442393bed9e
grian <grianguy32@gmail.com>
2025-06-04 02:08:55 +0300

    perf: day4 multithreading/better str checking

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("io.github.classgraph:classgraph:4.8.174")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 }
 
 tasks.test {
--- src/main/kotlin/year2015/day4/Day4.kt
+++ src/main/kotlin/year2015/day4/Day4.kt
@@ -1,40 +1,58 @@
 package me.grian.year2015.day4
 
+import kotlinx.coroutines.*
 import me.grian.Day
 import me.grian.util.getInputText
 import java.security.MessageDigest
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
 
 class Day4 : Day {
     override val input: String
         get() = getInputText(2015, 4)
 
-    override fun partOne(): String {
-        var md5 = ""
-        var i = 0
+    override fun partOne(): String = common(false)
 
-        while (!md5.startsWith("00000")) {
-            i++
-            md5 = md5("$input$i")
+    override fun partTwo(): String = common(true)
+
+    private fun common(p2: Boolean): String {
+        val goodNumber = AtomicInteger()
+        val found = AtomicBoolean()
+
+        runBlocking {
+            withContext(Dispatchers.Default) {
+                coroutineScope {
+                    val numThreads = Runtime.getRuntime().availableProcessors()
+
+                    repeat(numThreads) {
+                        launch {
+                            var i = it
+                            // some weird quirK of MessageDigest prob but this is required for p1 to work lol so i cant
+                            // get md in function, probably faster this way but i doubt it makes much of a diff..
+                            val md = MessageDigest.getInstance("MD5")
+                            while (!found.get()) {
+                                val md5 = md5(md, "$input$i")!!
+                                val byte: Byte = 0
+
+                                if (md5[0] == byte && md5[1] == byte && ((p2 && md5[2] == byte) || (!p2 && md5[2].toInt() and 0xFF < 16))) {
+                                    found.set(true)
+                                    goodNumber.set(i)
+                                    break
+                                }
+                                i += numThreads
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-        return i.toString()
+
+        return goodNumber.get().toString()
     }
 
-    override fun partTwo(): String {
-        var md5 = ""
-        var i = 0
-
-        while (!md5.startsWith("000000")) {
-            i++
-            md5 = md5("$input$i")
-        }
-
-        return i.toString()
-    }
-
-    private fun md5(input: String): String {
-        val md = MessageDigest.getInstance("MD5")
+    private fun md5(md: MessageDigest, input: String): ByteArray? {
         val digest = md.digest(input.toByteArray())
-        return digest.joinToString("") { "%02x".format(it) }
+        return digest
     }
 }
