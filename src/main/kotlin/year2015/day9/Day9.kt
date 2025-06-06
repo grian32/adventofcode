1386005a9226082153877829c5097de289027f5a
grian <grianguy32@gmail.com>
2025-06-06 19:27:45 +0300

    feat: d9 2015 p2

--- src/main/kotlin/year2015/day9/Day9.kt
+++ src/main/kotlin/year2015/day9/Day9.kt
@@ -4,16 +4,21 @@ import me.grian.Day
 import me.grian.util.WeightedGraph
 import me.grian.util.getInputText
 import me.grian.util.permuteUnique
+import kotlin.math.max
 import kotlin.math.min
 
 class Day9 : Day {
     override val input: String
         get() = getInputText(2015, 9)
 
-    override fun partOne(): String {
+    override fun partOne(): String = common(true).toString()
+
+    override fun partTwo(): String = common(false).toString()
+
+    private fun common(smallest: Boolean): Int {
         val graph = parseInput()
         val allPermutations = graph.getAllNodes().toList().permuteUnique()
-        var smallest = Int.MAX_VALUE
+        var output = if (smallest) Int.MAX_VALUE else Int.MIN_VALUE
 
         for (perm in allPermutations) {
             var totalDistance = 0
@@ -22,14 +27,10 @@ class Day9 : Day {
                 totalDistance += graph.getWeight(perm[i] , perm[i+1]) ?: 0
             }
 
-            smallest = min(totalDistance, smallest)
+            output = if (smallest) min(totalDistance, output) else max(totalDistance, output)
         }
 
-        return smallest.toString()
-    }
-
-    override fun partTwo(): String {
-        TODO("Not yet implemented")
+        return output
     }
 
     private fun parseInput(): WeightedGraph<String> {
