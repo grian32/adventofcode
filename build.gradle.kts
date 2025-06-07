48655fe0067dc3ac939e3c8b8f2fdc561e7b97b8
grian <grianguy32@gmail.com>
2025-06-07 23:40:06 +0300

    feat(2015/12): p2 soln :DDD

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.1.21"
 }
 
 group = "me.grian"
@@ -13,6 +14,7 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("io.github.classgraph:classgraph:4.8.174")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
 }
 
 tasks.test {
--- src/main/kotlin/year2015/day12/Day12.kt
+++ src/main/kotlin/year2015/day12/Day12.kt
@@ -1,5 +1,6 @@
 package me.grian.year2015.day12
 
+import kotlinx.serialization.json.*
 import me.grian.Day
 import me.grian.util.getInputText
 
@@ -10,11 +11,40 @@ class Day12 : Day {
     override fun partOne(): String = input.getSumOfNumbers().toString()
 
     override fun partTwo(): String {
-        val filteredString = ""
-
-        // todo filter out objects containing red
+        val filteredString = Json.encodeToString(Json.parseToJsonElement(input).process())
 
         return filteredString.getSumOfNumbers().toString()
+    }
+
+    private fun JsonElement.containsRed(): Boolean {
+        return when (this) {
+            is JsonObject -> values.any { it is JsonPrimitive && it.isString && it.content == "red" }
+            is JsonArray -> any { it.containsRed() }
+            is JsonPrimitive -> content.contains("red")
+        }
+    }
+
+    private fun JsonElement.process(): JsonElement? {
+        return when (this) {
+            is JsonObject -> {
+                if (containsRed()) return null
+
+                val map = mutableMapOf<String, JsonElement>()
+
+                for ((k, v) in this) {
+                    val processed = v.process()
+
+                    if (processed != null) map[k] = processed
+                }
+
+                if (map.isEmpty()) null else JsonObject(map)
+            }
+            is JsonArray -> {
+                val filtered = mapNotNull { it.process() }
+                if (filtered.isEmpty()) null else JsonArray(filtered)
+            }
+            else -> if (containsRed()) null else this
+        }
     }
 
     private fun String.getSumOfNumbers(): Int {
