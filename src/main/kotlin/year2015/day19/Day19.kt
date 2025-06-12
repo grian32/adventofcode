45cb4fb67488c366dc7ce7b290fe3f917a2434ee
grian <grianguy32@gmail.com>
2025-06-12 15:55:44 +0300

    feat(2015/19): p1 soln

---
+++ src/main/kotlin/year2015/day19/Day19.kt
@@ -0,0 +1,53 @@
+package me.grian.year2015.day19
+
+import me.grian.Day
+import me.grian.util.getInputText
+
+class Day19 : Day {
+    override val input: String
+        get() = getInputText(2015, 19)
+
+    override fun partOne(): String {
+        val (replacements, starter) = parseInput()
+        val distinctMolecules = mutableSetOf<String>()
+
+        for (i in replacements) {
+            for (j in starter.indices) {
+                // required so i can return early when it's n/a
+                val test = when (i.first.length) {
+                    1 -> starter[j].toString()
+                    2 -> {
+                        if (starter.getOrNull(j + 1) == null) continue
+                        starter[j] + starter[j+1].toString()
+                    }
+                    else -> continue
+                }
+
+                if (test == i.first) {
+                    distinctMolecules.add(
+                        starter.substring(0, j) + i.second + starter.substring(j + i.first.length)
+                    )
+                }
+            }
+        }
+
+        return distinctMolecules.size.toString()
+    }
+
+    override fun partTwo(): String {
+        TODO("Not yet implemented")
+    }
+
+    // replacements, starter
+    fun parseInput(): Pair<List<Pair<String, String>>, String> {
+        val input = input.split("\n\n")
+        val starter = input[1]
+
+        val replacements = input[0]
+            .split("\n")
+            .map { it.split(" => ") }
+            .map { it[0] to it[1] }
+
+        return replacements to starter
+    }
+}
